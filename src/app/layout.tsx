9fdba76cc7aa75457c385f44e9cb7b5fa0116c30
Jatin Maurya <mauryajatin45@gmail.com>
2025-12-11 13:47:51 +0530

    feat: Implement initial root layout with global styles, comprehensive SEO metadata, analytics integration, and core UI components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,30 +125,28 @@ export default function RootLayout({
             });
           `}
         </Script>
-        {/* Meta Pixel (Facebook Pixel) */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1256998902924411');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1256998902924411&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
+        {/* Meta Pixel (Facebook Pixel) - Using dangerouslySetInnerHTML for reliable execution */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1256998902924411');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1256998902924411&ev=PageView&noscript=1" />`,
+          }}
+        />
       </head>
       <body className={`${inter.className} min-h-screen bg-[#edf2f4] text-[#2b2d42] antialiased`} suppressHydrationWarning>
         <MetaPixel />
